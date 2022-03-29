ceb29f0a549d28381dab62076c10411fc7510a77
Justin Robertson <justin.keith.robertson@gmail.com>
2022-03-29 12:41:12 -0500

    Setup testing

---
+++ tests/index.test.ts
@@ -0,0 +1,7 @@
+import { test } from '../src/index';
+
+describe('When running the initial test', () => {
+    it('it should return a string', () => {
+        expect(typeof test()).toBe('string');
+    })
+})
